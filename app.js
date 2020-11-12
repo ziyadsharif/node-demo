75e0522ed9e21cbca611fb66b9698a08eb27404d
ziyadshariftoggle <65122707+ziyadshariftoggle@users.noreply.github.com>
2020-11-12 15:31:50 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/',(req,res) => res.send('Hello World!!!!'));
+
+app.listen (port, ()=> console.log('Server is running on port'+ port));
